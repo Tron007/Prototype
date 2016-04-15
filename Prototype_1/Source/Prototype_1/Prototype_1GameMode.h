2d8d3386ab54ae7f53cee0764486e4b3f0f065de
Judge <juudgespy@gmail.com>
2016-04-15 12:26:28 +0600

    Start of empty project

---
+++ Prototype_1/Source/Prototype_1/Prototype_1GameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "Prototype_1GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROTOTYPE_1_API APrototype_1GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
